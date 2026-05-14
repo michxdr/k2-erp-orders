3b09012039d6bb272c999a0c5f60687a734cf0df
Nikita Korpan <nikitakorpan9@gmail.com>
2026-05-14 18:21:00 +0300

    Initial project structure

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+db = SQLAlchemy()
+
+def create_app(__name__):
+    app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URL',
+        'postgresql://postgres:postgres@localhost:5432/k2_orders'
+    )
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    from app.routes import bp
+    app.register_blueprint(bp)
+
+    with app.app_context():
+        from app import models
+        db.create_all()
+
+    return app
