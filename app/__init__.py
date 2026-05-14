5e3e5b34aa4360987ccdc0f9a7007a6c705958b2
Nikita Korpan <nikitakorpan9@gmail.com>
2026-05-14 19:04:36 +0300

    Fix Flask host binding for Docker

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 db = SQLAlchemy()
 
-def create_app(__name__):
+def create_app():
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
